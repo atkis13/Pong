26baafc983bcdf86ff9e56cf5f3bcfc1b75ff2f4
atkis13 <kis_attila_1349@yahoo.com>
2020-04-19 11:13:37 +0300

    commit 1
    Created classes, added variables, created blueprints

---
+++ Source/Pong/PongGameMode.h
@@ -0,0 +1,43 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "PongGameMode.generated.h"
+
+
+class ABall;
+class UGameHUD;
+
+/**
+ * 
+ */
+UCLASS()
+class PONG_API APongGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+
+protected:
+
+	UPROPERTY(BlueprintReadWrite, Category = "References", meta = (BlueprintProtected))
+	UGameHUD* GameHUDRef;
+
+	UPROPERTY(BlueprintReadWrite, Category = "References", meta = (BlueprintProtected))
+	ABall* BallRef;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Float", meta = (BlueprintProtected))
+	float Direction = -1;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Float", meta = (BlueprintProtected))
+	float Ballspeed = 1400.0f;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Score", meta = (BlueprintProtected))
+	int PlayerScore = 0;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Score", meta = (BlueprintProtected))
+	int AIScore = 0;
+
+};
